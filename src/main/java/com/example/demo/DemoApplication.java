c5d261a5d60b5a23c45b59d406c28a869e436b36
KaranRohra <rohrakaran38@gmail.com>
2021-09-09 09:26:41 +0530

    [Spring]: Added setter and constructor injection

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		try (context) {
+			// User user = (User) context.getBean("setter-injection");
+			User user = (User) context.getBean("constructor-injection");
+			System.out.println(user);
+		}
+	}
+
+}
