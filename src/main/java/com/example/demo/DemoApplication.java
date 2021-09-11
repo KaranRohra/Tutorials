922846490e1f311f2f4e081bd08b904ed79a5242
KaranRohra <rohrakaran38@gmail.com>
2021-09-11 17:43:43 +0530

    [Spring]: Added inner bean

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 		try (context) {
-			// User user = (User) context.getBean("setter-injection");
-			 User user = (User) context.getBean("constructor-injection");
-			//User user = (User) context.getBean("object-injection");
+			User user = (User) context.getBean("setter-injection");
+			// User user = (User) context.getBean("constructor-injection");
+			// User user = (User) context.getBean("object-injection");
 			System.out.println(user);
 		}
 	}
